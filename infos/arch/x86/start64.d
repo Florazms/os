337b7f4b78638324d24497782f05982911f3df01
Florazms <55553879+Florazms@users.noreply.github.com>
2021-03-12 17:25:36 +0000

    Add files via upload

---
+++ infos/arch/x86/start64.d
@@ -0,0 +1,2 @@
+/afs/inf.ed.ac.uk/user/s20/s2002579/os-coursework/infos/arch/x86/start64.o: \
+ /afs/inf.ed.ac.uk/user/s20/s2002579/os-coursework/infos/arch/x86/start64.S
---
+++ infos/arch/x86/util/fast-string.d
@@ -0,0 +1,2 @@
+/afs/inf.ed.ac.uk/user/s20/s2002579/os-coursework/infos/arch/x86/util/fast-string.o: \
+ /afs/inf.ed.ac.uk/user/s20/s2002579/os-coursework/infos/arch/x86/util/fast-string.S
